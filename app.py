786b1a2f463dbd729e1d65e0fdabeaa045f099a6
ayadeissa <ayadeissa24@gmail.com>
2026-08-26 02:24:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,9 +69,9 @@ if hero_path.exists():
         <div class="hero-section">
             <div>
                 <div class="hero-title">BANQUE MISR MODEL</div>
-        </div>
+            </div>
                 
-        </div>
+            </div>
                  
                 
         """,
