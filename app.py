c5065369cfc3d78702d0b239f20cf987bd6eb7b6
ayadeissa <ayadeissa24@gmail.com>
2026-08-26 02:42:08 +0300

    Change border radius from 50px to 150px

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ if hero_path.exists():
             background-position: center;
             background-repeat: no-repeat;
             min-height: 700px;
-            border-radius: 50px;
+            border-radius: 150px;
             
             padding: 45px;
             display: flex;
