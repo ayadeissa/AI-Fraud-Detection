986475ac357e952f8bfceea7f537c9e7f07c6f3e
ayadeissa <ayadeissa24@gmail.com>
2026-08-26 01:10:54 +0300

    Increase minimum height from 350px to 500px

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if hero_path.exists():
             background-size: 100%;
             background-position: center;
             background-repeat: no-repeat;
-            min-height: 350px;
+            min-height: 500px;
             border-radius: 24px;
             padding: 45px;
             display: flex;
