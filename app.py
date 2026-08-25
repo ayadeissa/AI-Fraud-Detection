d60a3c9236aaee8bae90f94dcb5a0ff3ef5937e7
ayadeissa <ayadeissa24@gmail.com>
2026-08-26 01:11:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if hero_path.exists():
             background-size: 100%;
             background-position: center;
             background-repeat: no-repeat;
-            min-height: 500px;
+            min-height: 650px;
             border-radius: 24px;
             padding: 45px;
             display: flex;
