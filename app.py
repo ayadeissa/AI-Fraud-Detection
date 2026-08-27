58d4a3202eda19aea6a30251ef8d6bda6005ecf2
ayadeissa <ayadeissa24@gmail.com>
2026-08-27 03:04:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ if hero_path.exists():
             font-weight: 800;
             color: #FFC107 !important;
             text-align: left;
-            margin-bottom: -450px;
+            margin-top: -450px;
         }}
 
         
