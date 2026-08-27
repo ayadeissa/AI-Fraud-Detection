08827cd78debc0a18e55761349552bed84c1a3e6
ayadeissa <ayadeissa24@gmail.com>
2026-08-27 03:01:23 +0300

    Adjust margin-bottom in CSS style

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ if hero_path.exists():
             font-weight: 800;
             color: #FFC107 !important;
             text-align: center;
-            margin-bottom: -59px;
+            margin-bottom: -90px;
         }}
 
         
